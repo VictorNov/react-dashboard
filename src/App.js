068057adce16ce7aa8382027155b228c1be3c83d
Victor_Nov <victornov@bk.ru>
2022-05-24 19:57:00 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
-function App() {
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
+import { ECommerce, Area, Calendar, Bar, ColorMapping, ColorPicker,
+  Editor, Pie, Customers, Employees, Kanban, Financial, Orders,
+  Pyramid, Line, Stacked } from './pages'
+import './App.css'
+
+const App = () => {
+  const activeMenu = true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg'>
+
+        {/* Кнопка настроек */}
+        <div className='fixed right-4 bottom-4'
+             style={{ zIndex: '1000' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <TooltipComponent content='Settings' position='top'>
+            <button
+              type='button'
+              className='text-3xl p-3
+              hover:drop-shadow-xl
+              hover:bg-light-gray text-white'
+              style={{
+                background: 'blue',
+                borderRadius: '50%'
+            }}
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+
+        {/* Сайдбар */}
+        {activeMenu ? (
+          <div className='w-72 fixed sidebar bg-white
+          dark:bg-secondary-dark-bg'
+          >
+            <Sidebar />
+          </div>
+        ) : (
+          <div className='w-0
+          dark:bg-secondary-dark-bg'
+          >
+            <Sidebar />
+          </div>
+        )}
+
+        {/* Навигационная панель */}
+        <div className={
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full
+          ${activeMenu ? ' md:ml-72' : ' flex-2'}`
+        }>
+          <div className='fixed md:static bg-main-bg
+          dark:bg-main-dark-bg navbar w-full'
+          >
+            <Navbar />
+          </div>
+        </div>
+
+        {/* Основной контент страницы */}
+        <div>
+          <Routes>
+            {/* Dashboard */}
+            <Route path='/' element={ <ECommerce /> } />
+            <Route path='/ecommerce' element={ <ECommerce /> } />
+
+            {/* Страницы */}
+            <Route path='/orders' element={ <Orders /> } />
+            <Route path='/employees' element={ <Employees /> } />
+            <Route path='/customers' element={ <Customers /> } />
+
+            {/* Приложения */}
+            <Route path='/kanban' element={ <Kanban /> } />
+            <Route path='/editor' element={ <Editor /> } />
+            <Route path='/calendar' element={ <Calendar /> } />
+            <Route path='/color-picker' element={ <ColorPicker /> } />
+
+            {/* Графики */}
+            <Route path='/line' element={ <Line /> } />
+            <Route path='/area' element={ <Area /> } />
+            <Route path='/bar' element={ <Bar /> } />
+            <Route path='/pie' element={ <Pie /> } />
+            <Route path='/financial' element={ <Financial /> } />
+            <Route path='/color-mapping' element={ <ColorMapping /> } />
+            <Route path='/pyramid' element={ <Pyramid /> } />
+            <Route path='/stacked' element={ <Stacked /> } />
+          </Routes>
+        </div>
+
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SparkLine = () => {
+  return (
+    <div>
+      Spark Line Chart
+    </div>
+  )
+}
+
+export default SparkLine
